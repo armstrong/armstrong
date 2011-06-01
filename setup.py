a3b9e2a8090b96f292eea0d0b17e21eb49ce1be1
Travis Swicegood <development@domain51.com>
2011-06-01 18:06:46 -0500

    update to be alpha.0 of next release for dev version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 if os.path.exists("MANIFEST"):
     os.unlink("MANIFEST")
 
-VERSION = ("11", "06", "0", "beta", "1")
+VERSION = ("11", "09", "0", "alpha", "0")
 
 setup(
     name='armstrong',
